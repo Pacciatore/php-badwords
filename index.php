12428ffb38502cd31103e53009140e2f3a67e098
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-12 18:48:40 +0200

    censura parola in get dal paragrafo

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
 
-$testText = '<p> Sono un paragrafo di testo </p>'
+$testText = 'Sono una frase, con delle parole che successivamente vengono censurate.';
+$bannedWord = $_GET['toBan'];
+$pCensored = str_replace($bannedWord, "***", $testText);
+
 
 ?>
 
-<?= $testText . 'Lunghezza del paragrafo: ' . strlen($testText) ?>
+<p> <?= $testText ?> </p>
+
+<p> <?= 'Lunghezza del paragrafo: ' . strlen($testText) ?> </p>
+
+<p> Censura della parola: <?= $bannedWord ?> </p>
+
+<p> Testo con censura: <?= $pCensored ?> </p>
